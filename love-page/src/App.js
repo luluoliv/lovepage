37aaf32f28f21f255a607b040598a121eb72f5d3
Luara <luaralima205@gmail.com>
2022-12-11 19:41:43 -0300

    lovingDevs arquivos

--- love-page/src/App.js
+++ love-page/src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="login-container">
+      <div className="center">
+        <div className="login-container">
+          <div className="login-photo"></div>
+          <input className="login-senha" type="password" name="senha" placeholder="Senha"></input>
+        </div>
       </div>
     </div>
   );
