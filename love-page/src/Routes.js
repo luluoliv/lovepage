8c7515807f38b922e5ecdeb9b564a46c32ccefcf
Luara <luaralima205@gmail.com>
2022-12-13 19:30:47 -0300

    LovingDevs

---
+++ love-page/src/Routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+export default () => {
+    return (
+        <Routes>
+            <Route exact path="/">
+
+            </Route>
+            <Route exact path="/">
+
+            </Route>
+        </Routes>
+    )
+}
