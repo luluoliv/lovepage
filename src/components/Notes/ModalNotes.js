6cb20d81ee99ba63bf617cfae7bf720af94bed6f
Luara Lima <luaralima205@gmail.com>
2023-05-22 18:33:20 -0300

    notes scroll

--- src/components/Notes/ModalNotes.js
+++ src/components/Notes/ModalNotes.js
@@ -1,49 +1,55 @@
 import React from "react";
 import { Form, Button, Modal } from "react-bootstrap";
 import { useEffect, useState } from "react";
-import {useNavigate} from 'react-router-dom';
 import axios from "axios";
 
 import "./ModalNotes.css";
 
 function ModalNotes(props) {
-
     const [title, setTitle] = useState(false);
     const [user, setUser] = useState(false);
 
-    const postNote = async () =>{
-        const data ={
+    const postNote = async () => {
+        const data = {
             title: title,
-            state: '0',
-            user: localStorage.getItem('user_id')
-        }
+            state: "0",
+            user: localStorage.getItem("user_id"),
+        };
 
         try {
-            await axios.post('https://love-pageapi.onrender.com/notes/', JSON.stringify(data), {
-              headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Token '+localStorage.getItem('token'),
-              },
-              withCredentials: true,
-            })
-            .then((data)=>{
-                console.log(data)
-            }).then(()=>{
-                ModalNotes.hide()
-            })
+            await axios.post(
+                "https://love-pageapi.onrender.com/notes/",
+                JSON.stringify(data),
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: "Token " + localStorage.getItem("token"),
+                    },
+                    withCredentials: true,
+                }
+            );
+            props.onHide();
+            alert("Enviado com sucesso!");
         } catch (error) {
-            console.error('Error:', error);
-            alert('Erro / Auth error')
-
+            console.error("Error:", error);
+            alert("Erro / Auth error");
         }
-    }
+        /* .then((data) => {
+                    console.log(data);
+                })
+                .then(() => {
+                    ModalNotes.hide();
+                });
+        } catch (error) {
+            console.error("Error:", error);
+            alert("Erro / Auth error");
+        } */
+    };
 
     return (
         <Modal
             className="modal-note"
-            {
-            ...props
-            }
+            {...props}
             aria-labelledby="contained-modal-title-vcenter"
             centered
             size="md"
@@ -59,7 +65,6 @@ function ModalNotes(props) {
             </Modal.Header>
             <Modal.Body>
                 <Form>
-
                     <Form.Group
                         className="mb-3"
                         controlId="exampleForm.ControlTextarea1"
@@ -68,7 +73,9 @@ function ModalNotes(props) {
                         <Form.Control
                             className="modal-note-textarea"
                             as="textarea"
-                            onChange={(e) => {setTitle(e.target.value)}}
+                            onChange={(e) => {
+                                setTitle(e.target.value);
+                            }}
                             rows={1}
                         />
                     </Form.Group>
@@ -78,21 +85,25 @@ function ModalNotes(props) {
                         controlId="exampleForm.ControlTextarea1"
                     >
                         <Form.Check
-                          inline
-                          label="Guizen"
-                          name="group1"
-                          value={'Guilherme'}
-                          type='radio'
-                          onChange={(e) => {setUser(e.target.value)}}
+                            inline
+                            label="Guizen"
+                            name="group1"
+                            value={"Guilherme"}
+                            type="radio"
+                            onChange={(e) => {
+                                setUser(e.target.value);
+                            }}
                         />
                         <Form.Check
-                          inline
-                          label="Lulu"
-                          name="group1"
-                          value={'Luara'}
-                          radio
-                          type='radio'
-                          onChange={(e) => {setUser(e.target.value)}}
+                            inline
+                            label="Lulu"
+                            name="group1"
+                            value={"Luara"}
+                            radio
+                            type="radio"
+                            onChange={(e) => {
+                                setUser(e.target.value);
+                            }}
                         />
                     </Form.Group>
                     <Button
