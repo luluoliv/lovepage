0ef56c13496a7bb5b338e71f40a7e76da7d82dd6
Luara Lima <luaralima205@gmail.com>
2023-05-27 15:23:50 -0300

    photo on gallery edited

--- src/components/Gallery/Photo.js
+++ src/components/Gallery/Photo.js
@@ -2,28 +2,61 @@ import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-import "./../../components/Gallery/Photo.css"
+import "./../../components/Gallery/Photo.css";
 
 function Photo() {
     const [photos, setPhotos] = useState([]);
 
+
     useEffect(() => {
-        axios
-            .get("https://love-pageapi.onrender.com/features/")
-            .then((responseData) => {
-                setPhotos(responseData.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    "https://love-pageapi.onrender.com/features/",
+                    {
+                        headers: {
+                            Authorization:
+                                "Token " + localStorage.getItem("token"),
+                        },
+                    }
+                );
+                setPhotos(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchData();
     }, []);
+
+    const [hoveredPhotoId, setHoveredPhotoId] = useState(null);
+
+    const handleMouseEnter = (photoId) => {
+        setHoveredPhotoId(photoId);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredPhotoId(null);
+    };
+
     return (
         <div className="photo-gallery">
             {photos && photos.length > 0 ? (
                 photos.map((photo) => (
-                    <div key={photo.id} className="photo-item">
-                        <img className="photo-img" src={photo.photo} alt={photo.desc} />
-                        <p className="photo-desc">{photo.desc}</p>
+                    <div
+                        key={photo.id}
+                        className="photo-item"
+                        onMouseEnter={() => handleMouseEnter(photo.id)}
+                        onMouseLeave={handleMouseLeave}
+                    >
+                        <img
+                            className="photo-img"
+                            src={photo.photo}
+                            alt={photo.desc}
+                        />
+                        {hoveredPhotoId === photo.id && (
+                            <p className="photo-desc">{photo.desc}</p>
+                        )}
                     </div>
                 ))
             ) : (
